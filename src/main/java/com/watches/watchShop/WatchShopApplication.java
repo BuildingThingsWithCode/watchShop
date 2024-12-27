b255d41b0ccb3dbaf1c9739ab9116093d849cc94
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-27 23:57:47 +0100

    Connect project to database hosted on https://www.koyeb.com/ and verify connection

--- src/main/java/com/watches/watchShop/WatchShopApplication.java
+++ src/main/java/com/watches/watchShop/WatchShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controllers"})
+@ComponentScan(basePackages = {"controllers", "service"})
 public class WatchShopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/model/Watch.java
+++ src/main/java/model/Watch.java
@@ -1,5 +1,12 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
 public class Watch {
 	
 	private String brand;
