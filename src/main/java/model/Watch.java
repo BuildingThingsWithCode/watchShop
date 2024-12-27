cac4217497e2610163f10cdb32a92eda920ca92b
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-27 02:03:22 +0100

    Group style elements

--- src/main/java/model/Watch.java
+++ src/main/java/model/Watch.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Watch {
-
+	
+	private String brand;
+	private String movement;
+	private double caseSize;
+	private double height;
+	private double lugToLug;
+	private double lugWidth;
+	private double color;
+	private int waterResistance;
+	private String crystal;
+	private String caseMaterial;
+	private String bandMaterial;
 }
