c7aee39410ca91a711bd14235b3521a0babeda78
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-28 16:28:47 +0100

    Delete hastly created classes

--- src/main/java/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import java.awt.Image;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
