489fbdc86dc60aa97fe570717b9f9bfd9e001d9d
Lars Martens <BuildingThingsWithCode@gmail.com>
2024-12-30 04:09:21 +0100

    Change logo, fix issue with home.html file

--- src/test/java/com/watches/watchShop/WatchShopApplicationTests.java
+++ src/test/java/com/watches/watchShop/WatchShopApplicationTests.java
@@ -3,7 +3,10 @@ package com.watches.watchShop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.watchShop.WatchShopApplication;
+
+
+@SpringBootTest(classes = WatchShopApplication.class)
 class WatchShopApplicationTests {
 
 	@Test
